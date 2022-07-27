93a8364026862f1269db5be5ce75471361d9e952
Natalí Rodrigues <natalyrodrigues43210@gmail.com>
2022-07-27 18:14:23 -0300

    Criando classe em JAVA

---
+++ src/main/java/com/mycompany/aprendendo_java_netbeans/Aprendendo_Java_netbeans.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.aprendendo_java_netbeans;
+
+/*
+ * @author Natalí
+ */
+public class Aprendendo_Java_netbeans {
+
+    public static void main(String[] args) {
+        
+    }
+}
